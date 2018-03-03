1923b270f5adc70a1ee7920a9dcec155ae6e3b9e
Jaimoto Flautero <jaimoto81@gmail.com>
2018-03-03 18:16:13 -0500

    Commit inicial, random ok

---
+++ app/src/main/java/demo/kotlin/jaimoto/kotlindemo/MainActivity.kt
@@ -0,0 +1,32 @@
+package demo.kotlin.jaimoto.kotlindemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+/**
+ * Ejemplo de app de Android Kotlin Beginner Tutorial (Google I/O '17)
+ * https://www.youtube.com/watch?v=sZWMPYIkNd8
+ */
+class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Poke","Home Burguer","Teriyaki","Da Quei Matti","KFC","McDonalds","Panacleta","Tostao","A Cocinar!!! ")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //1. referenciar los elementos del layout sin usar el findByID
+
+
+        decideButton.setOnClickListener {
+            println("click en buton")
+            var random = Random()
+            val randomFood = Random().nextInt(foodList.count())
+            selectedFoodTxt.text = foodList.elementAt(randomFood)
+        }
+
+
+    }
+}
